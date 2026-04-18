9d69367c4582bcabda8273f2d81b10ec5830e13d
Dev Kumar <devkmandal0@gmail.com>
2026-04-18 11:50:39 +0530

    Extraction of Number

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -1,5 +1,15 @@
 public class BasicMath {
     static void main() {
         System.out.println("This is Basic math");
+        extractionOfNumber();
+    }
+
+    public static void extractionOfNumber(){
+        int number = 456789;
+        while (number>0){
+            int extractedNumber = number%10;
+            System.out.println(extractedNumber);
+            number = number/10;
+        }
     }
 }
