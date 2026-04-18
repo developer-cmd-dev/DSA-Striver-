1f92fb54809f9aa74f27602860121dc30c47de20
Dev Kumar <devkmandal0@gmail.com>
2026-04-18 11:53:25 +0530

    count of digits

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -1,7 +1,8 @@
 public class BasicMath {
     static void main() {
         System.out.println("This is Basic math");
-        extractionOfNumber();
+//        extractionOfNumber();
+        CountDigits();
     }
 
     public static void extractionOfNumber(){
@@ -11,5 +12,17 @@ public class BasicMath {
             System.out.println(extractedNumber);
             number = number/10;
         }
+    }
+
+    public static void CountDigits(){
+        int number = 456789;
+        int count = 0;
+        while (number>0){
+            int extractedNumber = number%10;
+            count++;
+            number = number/10;
+        }
+
+        System.out.println(count);
     }
 }
