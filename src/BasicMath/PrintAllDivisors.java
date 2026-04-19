073a00deef45af73278e690f01973b2a6c32ee27
Dev Kumar <devkmandal0@gmail.com>
2026-04-19 11:12:35 +0530

    Print all divisors in optimised way

--- src/BasicMath/PrintAllDivisors.java
+++ src/BasicMath/PrintAllDivisors.java
@@ -1,34 +1,30 @@
 package BasicMath;
 
+import java.util.*;
+
 public class PrintAllDivisors {
 
     static void main() {
 
-        int x = 45;
+        int x = 36;
 
         optimised(x);
-
-
 
     }
 
     public static void optimised(int x){
 
-
         for (int i=1;i<=Math.sqrt(x);i++){
 
             if((x%i)==0){
                 System.out.println(i);
-
                 if((x/i)!=i){
-                    System.out.println(x/i);
+                    System.out.println(i);
                 }
             }
 
 
         }
-
-
 
     }
 }
