1c6b389539208d2ff159e2177fbfe6e6741c0369
Dev Kumar <devkmandal0@gmail.com>
2026-04-19 10:30:18 +0530

    Print all divisors

---
+++ src/BasicMath/PrintAllDivisors.java
@@ -0,0 +1,22 @@
+package BasicMath;
+
+public class PrintAllDivisors {
+
+    static void main() {
+
+        int x = 36;
+
+        for (int i=1;i<=x;i++){
+
+            if((x%i)==0){
+                System.out.println(i);
+            }
+
+
+        }
+
+
+
+
+    }
+}
